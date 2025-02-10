f283bed0e028c44d90295c348ce7ac3cb42ac471
QusaiSak <144017972+QusaiSak@users.noreply.github.com>
2025-02-10 22:06:02 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,19 +27,19 @@ const socialLinks = [
 const Footer = () => {
   return (
     <footer className="w-screen bg-[#5542ff] py-4 text-black">
-      <div className="container flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+      <div className="container flex flex-col items-center justify-between mx-auto md:flex-row">
         <p className="text-center text-sm font-light md:text-left">
           &copy; {new Date().getFullYear()} All rights reserved.
         </p>
 
-        <div className="flex justify-center gap-4 md:justify-end">
+        <div className="flex gap-2 ">
           {socialLinks.map((link, index) => (
             <a
               key={index}
               href={link.href}
               target="_blank"
               rel="noopener noreferrer"
-              className="text-white transition-colors duration-500 ease-in-out hover:text-yellow-400">
+              className="text-black transition-colors duration-500 ease-in-out hover:text-yellow-400 md:text-xl">
               {link.icon}
             </a>
           ))}
